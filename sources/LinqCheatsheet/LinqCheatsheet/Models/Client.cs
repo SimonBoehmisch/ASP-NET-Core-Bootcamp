84ccd5bd3b5b9f44cbdd53a7ca92d76cea03fadf
Simon Böhmisch <si.boehmisch@gmail.com>
2023-02-05 17:52:12 +0100

    Add the LinqCheatSheet solution

---
+++ sources/LinqCheatsheet/LinqCheatsheet/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqCheatsheet.Models;
+
+public class Client
+{
+    public List<Case> Cases { get; set; } = default!;
+
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+}
